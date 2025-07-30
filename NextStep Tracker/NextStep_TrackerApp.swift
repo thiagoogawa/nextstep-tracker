ce9f438a886b6b7db69fd7b2912a57b0238dd02d
Thiago Ogawa <thiago.ogawa11@gmail.com>
2025-07-30 04:12:28 -0300

    init: project setup

---
+++ NextStep Tracker/NextStep_TrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  NextStep_TrackerApp.swift
+//  NextStep Tracker
+//
+//  Created by Thiago Ogawa on 30/07/25.
+//
+
+import SwiftUI
+
+@main
+struct NextStep_TrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
