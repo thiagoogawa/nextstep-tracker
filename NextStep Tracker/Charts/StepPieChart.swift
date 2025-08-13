b740b2628a29404a7e06f0c4516a92c9258fec1d
Thiago Ogawa <thiago.ogawa11@gmail.com>
2025-08-13 14:38:35 -0300

    pie chart interactivity

--- NextStep Tracker/Charts/StepPieChart.swift
+++ NextStep Tracker/Charts/StepPieChart.swift
@@ -9,8 +9,19 @@ import SwiftUI
 import Charts
 
 struct StepPieChart: View {
+    @State private var rawSelectedChartValue: Double? = 0
     
     var chartData: [WeekdayChartData]
+    
+    var selectedWeekday: WeekdayChartData? {
+        guard let rawSelectedChartValue else { return nil }
+        var total = 0.0
+        
+        return chartData.first {
+            total += $0.value
+            return rawSelectedChartValue <= total
+        }
+    }
     
     var body: some View {
         VStack(alignment: .leading) {
@@ -29,12 +40,34 @@ struct StepPieChart: View {
                 ForEach(chartData) { weekday in
                     SectorMark(angle: .value("Average steps", weekday.value),
                                innerRadius: .ratio(0.618),
-                               angularInset: 1)
+                               outerRadius: selectedWeekday?.date.weekdayInt == weekday.date.weekdayInt ? 140 : 110, angularInset: 1)
                     .foregroundStyle(.pink.gradient)
                     .cornerRadius(6)
+                    .opacity(selectedWeekday?.date.weekdayInt == weekday.date.weekdayInt ? 1 : 0.3)
                 }
             }
+            .chartAngleSelection(value: $rawSelectedChartValue.animation(.easeInOut))
             .frame(height: 240)
+            .chartBackground { proxy in
+                GeometryReader { geo in
+                    if let plotFrame = proxy.plotFrame {
+                        let frame = geo[plotFrame]
+                        if let selectedWeekday {
+                            VStack {
+                                Text(selectedWeekday.date.weekdayTitle)
+                                    .font(.title3.bold())
+                                    .contentTransition(.identity)
+                                
+                                Text(selectedWeekday.value, format: .number.precision(.fractionLength(0)))
+                                    .fontWeight(.medium)
+                                    .foregroundStyle(.secondary)
+                                    .contentTransition(.numericText())
+                            }
+                            .position(x: frame.midX, y: frame.midY)
+                        }
+                    }
+                }
+            }
         }
         .padding()
         .background(RoundedRectangle(cornerRadius: 12).fill(Color(.secondarySystemBackground)))
