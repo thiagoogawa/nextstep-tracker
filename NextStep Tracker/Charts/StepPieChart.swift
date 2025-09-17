b5e1146dd8b6a3383d17f1404fe838583e661128
Thiago Ogawa <thiago.ogawa11@gmail.com>
2025-09-17 20:18:14 -0300

    implement haptic feedback on charts

--- NextStep Tracker/Charts/StepPieChart.swift
+++ NextStep Tracker/Charts/StepPieChart.swift
@@ -10,6 +10,7 @@ import Charts
 
 struct StepPieChart: View {
     @State private var rawSelectedChartValue: Double? = 0
+    @State private var selectedDay: Date?
     
     var chartData: [WeekdayChartData]
     
@@ -71,6 +72,13 @@ struct StepPieChart: View {
         }
         .padding()
         .background(RoundedRectangle(cornerRadius: 12).fill(Color(.secondarySystemBackground)))
+        .sensoryFeedback(.selection, trigger: selectedDay)
+        .onChange(of: selectedWeekday) { oldValue, newValue in
+            guard let oldValue, let newValue else { return }
+            if oldValue.date.weekdayInt != newValue.date.weekdayInt {
+                selectedDay = newValue.date
+            }
+        }
     }
 }
 
--- NextStep Tracker/Charts/WeightLineChart.swift
+++ NextStep Tracker/Charts/WeightLineChart.swift
@@ -10,6 +10,7 @@ import Charts
 
 struct WeightLineChart: View {
     @State private var rawSelectedDate: Date?
+    @State private var selectedDay: Date?
     
     var selectedStat: HealthMetricContext
     var chartData: [HealthMetric]
@@ -94,6 +95,12 @@ struct WeightLineChart: View {
         }
         .padding()
         .background(RoundedRectangle(cornerRadius: 12).fill(Color(.secondarySystemBackground)))
+        .sensoryFeedback(.selection, trigger: selectedDay)
+        .onChange(of: rawSelectedDate) { oldValue, newValue in
+            if oldValue?.weekdayInt != newValue?.weekdayInt {
+                selectedDay = newValue
+            }
+        }
     }
     
     var annotationView: some View {
