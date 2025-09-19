b9c79ee55f61f7f16291a607ac200dfa3fdbe821
Thiago Ogawa <thiago.ogawa11@gmail.com>
2025-09-19 15:11:10 -0300

    text input validation

--- NextStep Tracker/Managers/HealthKitManager.swift
+++ NextStep Tracker/Managers/HealthKitManager.swift
@@ -13,6 +13,7 @@ enum STError: LocalizedError {
     case sharingDenied(quantityType: String)
     case noData
     case unableToCompleteRequest
+    case invalidValue
     
     var errorDescription: String? {
         switch self {
@@ -24,6 +25,8 @@ enum STError: LocalizedError {
             "No Data"
         case .unableToCompleteRequest:
             "Unable to Complete Request"
+        case .invalidValue:
+            "Invalid Value"
         }
     }
     
@@ -37,6 +40,8 @@ enum STError: LocalizedError {
             "There is no data for this Health statistic."
         case .unableToCompleteRequest:
             "We are unable to complete your request at this time.\n\nPlease try again later or contact support."
+        case .invalidValue:
+            "Must be a numeric value with a maximum of one decimal place."
         }
     }
 }
